d5dbbcae07bc577d49d72863540d4a8d0f382c1f
herbb100 <herbolwanga@gmail.com>
2024-06-16 20:40:01 +0300

    Home page most basic design compelet image added and h1 also some ccss styling done

--- src/home.js
+++ src/home.js
@@ -1,13 +1,28 @@
+import homePic from './home.jpg';
 
 export function addParaHome(){
     while(content.firstChild){
         content.firstChild.remove();
     };
 
-    const paraHome = document.createElement('p');
-    paraHome.textContent = "Dolor ipsum blah blah blah";
-    paraHome.classList.add('homeP');
-    
-    content.appendChild(paraHome);
+    const homeContent = document.createElement('div');
+
+    const homeLeft = document.createElement('div');
+    const homeLeftHeader = document.createElement('h1');
+
+    const homeRight = document.createElement('div');
+    const homePhoto = new Image();
+    homePhoto.src = homePic;
+
+    homeLeftHeader.textContent = "A FILLING MEAL THAT CAN LAST A WHOLE DAY";
+
+    homeContent.classList.add('homeContent')
+    homeLeft.classList.add('homeLeft');
+    homeRight.classList.add('homeRight');
+
+    homeRight.appendChild(homePhoto);
+    homeLeft.append(homeLeftHeader);
+    homeContent.append(homeLeft,homeRight);
+    content.appendChild(homeContent);
 
 };
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,11 @@ module.exports = {
                 test:/\.css$/i,
                 use:['style-loader','css-loader']
             },
-        ],
+            
+            {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+            },
+        ], 
     },
 }
